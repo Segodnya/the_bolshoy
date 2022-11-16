b32a54636416e907afcb076b23c91f607c484e63
Kirill Kurentsov <immortalsteel@gmail.com>
2022-11-16 16:40:31 +0300

    add local access for swiperjs

---
+++ js/swiper.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper__button_type_next",
+    prevEl: ".swiper__button_type_prev",
+  },
+});
