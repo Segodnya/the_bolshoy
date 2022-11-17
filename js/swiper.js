63ffc0b2c75e3f4d57e70df4e898d3ba45eca459
Kirill Kurentsov <immortalsteel@gmail.com>
2022-11-17 13:16:52 +0300

    add adaptive for 992px and above

--- js/swiper.js
+++ js/swiper.js
@@ -6,3 +6,13 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper__button_type_prev",
   },
 });
+
+var swiperProjects = new Swiper(".swiper__projects", {
+  slidesPerView: 1.5,
+  spaceBetween: 0,
+  centeredSlides: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
