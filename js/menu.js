10d4a01b1564ba63a151a4f8519e05634b9e868f
Kirill Kurentsov <immortalsteel@gmail.com>
2022-11-18 13:27:09 +0300

    add close-button js

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,11 @@
 let button = document.querySelector(".header-mobile__button");
-let menu = document.querySelector("menu");
+let closeButton = document.querySelector(".header-mobile__button");
+let menu = document.querySelector("menu__close");
 
 button.addEventListener("click", function () {
+  menu.classList.toggle("menu-active");
+});
+
+closeButton.addEventListener("click", function () {
   menu.classList.toggle("menu-active");
 });
