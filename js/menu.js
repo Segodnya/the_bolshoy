97411e263651fccc8af3638de7a58949b2fb096c
Kirill Kurentsov <immortalsteel@gmail.com>
2022-11-18 15:13:14 +0300

    add mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,11 @@
 let button = document.querySelector(".header-mobile__button");
-// let closeButton = document.querySelector(".header-mobile__button");
-let menu = document.querySelector("menu__close");
+let closeButton = document.querySelector(".menu__close");
+let menu = document.querySelector(".menu");
 
 button.addEventListener("click", function () {
   menu.classList.toggle("menu-active");
 });
 
-/* closeButton.addEventListener("click", function () {
+closeButton.addEventListener("click", function () {
   menu.classList.toggle("menu-active");
-}); */
+});
